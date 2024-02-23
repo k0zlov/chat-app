6bcb257ec2c74a3187f1f3c3f43beca45f22dc18
Morro <ivankozlov0624@gmail.com>
2024-02-23 21:08:15 +0100

    #5 Dependency Injection for Network, Users Provider

--- lib/di_container.dart
+++ lib/di_container.dart
@@ -1,8 +1,37 @@
+import 'package:chat_app/core/network/network.dart';
+import 'package:chat_app/core/network/network_implementation.dart';
+import 'package:chat_app/features/template_feature/data/providers/remote/users_provider.dart';
 import 'package:get_it/get_it.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 /// Get it instance
 final GetIt getIt = GetIt.instance;
 
+/// Registers dependencies required by the application asynchronously.
+///
+/// This method registers the network dependencies and providers needed by the
+/// application using the `getIt` service locator.
+/// - Calls `_network()` to initialize network-related dependencies.
+/// - Calls `_provider()` to initialize provider-related dependencies.
 Future<void> registerDependencies() async {
+  await _network();
+  _provider();
+}
 
+Future<void> _network() async {
+  const supabaseUrl = 'https://pyjduiurropblpsqzgzs.supabase.co';
+  const supabaseKey = String.fromEnvironment('SUPABASE_KEY');
+
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseKey,
+  );
+
+  getIt.registerLazySingleton<Network>(NetworkImpl.new);
+}
+
+void _provider() {
+  getIt.registerLazySingleton<UsersProvider>(
+    () => UserProviderImpl(network: getIt()),
+  );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,10 @@
 import 'package:chat_app/application.dart';
 import 'package:chat_app/di_container.dart';
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await registerDependencies();
-
-  /// Initializing Supabase
-  const supabaseUrl = 'https://pyjduiurropblpsqzgzs.supabase.co';
-  const supabaseKey = String.fromEnvironment('SUPABASE_KEY');
-  await Supabase.initialize(
-    url: supabaseUrl,
-    anonKey: supabaseKey,
-  );
 
   runApp(const MyApp());
 }
