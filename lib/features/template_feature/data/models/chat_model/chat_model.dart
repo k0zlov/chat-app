b29eeea2e736c0c1b7573a362c10fc873d4eee6a
Morro <ivankozlov0624@gmail.com>
2024-02-25 21:14:01 +0100

    #6 Adding documentation for models

--- lib/features/template_feature/data/models/chat_model/chat_model.dart
+++ lib/features/template_feature/data/models/chat_model/chat_model.dart
@@ -4,21 +4,25 @@ part 'chat_model.freezed.dart';
 
 part 'chat_model.g.dart';
 
+/// Represents a response containing a list of chat models.
 @freezed
 class ChatResponse with _$ChatResponse {
+  /// Default constructor for [ChatResponse].
   const factory ChatResponse({
     @JsonKey(name: 'items') @Default([]) List<ChatModel> chats,
   }) = _ChatResponse;
 
   const ChatResponse._();
 
+  /// Creates a [ChatResponse] instance from JSON data.
   factory ChatResponse.fromJson(Map<String, dynamic> json) =>
       _$ChatResponseFromJson(json);
 }
 
-
+/// Represents a chat model.
 @freezed
 class ChatModel with _$ChatModel {
+  /// Default constructor for [ChatModel].
   const factory ChatModel({
     @Default(0) int chatid,
     @JsonKey(name: 'chattype') @Default('') String chatType,
@@ -27,6 +31,7 @@ class ChatModel with _$ChatModel {
 
   const ChatModel._();
 
+  /// Creates a [ChatModel] instance from JSON data.
   factory ChatModel.fromJson(Map<String, dynamic> json) =>
       _$ChatModelFromJson(json);
 }
--- lib/features/template_feature/data/models/message_model/message_model.dart
+++ lib/features/template_feature/data/models/message_model/message_model.dart
@@ -4,21 +4,25 @@ part 'message_model.freezed.dart';
 
 part 'message_model.g.dart';
 
+/// Represents a response containing a list of message models.
 @freezed
 class MessageResponse with _$MessageResponse {
+  /// Default constructor for [MessageResponse].
   const factory MessageResponse({
     @JsonKey(name: 'items') @Default([]) List<MessageModel> messages,
   }) = _MessageResponse;
 
   const MessageResponse._();
 
+  /// Creates a [MessageResponse] instance from JSON data.
   factory MessageResponse.fromJson(Map<String, dynamic> json) =>
       _$MessageResponseFromJson(json);
 }
 
-
+/// Represents a message model.
 @freezed
 class MessageModel with _$MessageModel {
+  /// Default constructor for [MessageModel].
   const factory MessageModel({
     @Default(0) int messageid,
     @Default(0) int chatid,
@@ -29,6 +33,7 @@ class MessageModel with _$MessageModel {
 
   const MessageModel._();
 
+  /// Creates a [MessageModel] instance from JSON data.
   factory MessageModel.fromJson(Map<String, dynamic> json) =>
       _$MessageModelFromJson(json);
 }
