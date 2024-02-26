dd73714b1016fdee12a2dcc91b25139f9c5cddd8
Morro <ivankozlov0624@gmail.com>
2024-02-26 13:02:49 +0100

    #6 Changing tableName function to getter

--- lib/core/database/tables.dart
+++ lib/core/database/tables.dart
@@ -4,7 +4,7 @@
 /// such as `tableName`, `fields`, and `createTableQuery`.
 abstract class Table {
   /// Returns the name of the table.
-  String tableName();
+  String get tableName;
 
   /// Returns a map of field names and their SQL data types.
   Map<String, String> fields();
@@ -14,11 +14,11 @@ abstract class Table {
   /// Constructs the SQL `CREATE TABLE` statement using the table name and fields.
   /// Iterates over the map of fields to construct the field definitions part of the query.
   String createTableQuery() {
-    String fieldsQuery = fields()
+    final String fieldsQuery = fields()
         .entries
         .map((field) => '${field.key} ${field.value}')
         .join(', ');
-    return 'CREATE TABLE ${tableName()} ($fieldsQuery)';
+    return 'CREATE TABLE $tableName ($fieldsQuery)';
   }
 }
 
@@ -28,12 +28,12 @@ abstract class Table {
 /// with appropriate SQL data types and constraints such as `NOT NULL` and `PRIMARY KEY`.
 class UsersTable extends Table {
   @override
-  String tableName() => 'users';
+  String get tableName => 'users';
 
   @override
   Map<String, String> fields() => {
     'id': 'INTEGER PRIMARY KEY AUTOINCREMENT',
-    'userid': 'INTEGER',
+    'userid': 'INTEGER UNIQUE',
     'username': 'TEXT NOT NULL',
     'email': 'TEXT NOT NULL',
     'phonenumber': 'TEXT NOT NULL',
@@ -47,12 +47,12 @@ class UsersTable extends Table {
 /// Defines fields related to chat information, such as chat ID, chat type, and creation date.
 class ChatsTable extends Table {
   @override
-  String tableName() => 'chats';
+  String get tableName => 'chats';
 
   @override
   Map<String, String> fields() => {
     'id': 'INTEGER PRIMARY KEY AUTOINCREMENT',
-    'chatid': 'INTEGER',
+    'chatid': 'INTEGER UNIQUE',
     'chattype': 'TEXT NOT NULL',
     'createdat': 'TEXT NOT NULL',
   };
@@ -64,7 +64,7 @@ class ChatsTable extends Table {
 /// and user IDs, with foreign keys linking to the 'chats' and 'users' tables.
 class MessagesTable extends Table {
   @override
-  String tableName() => 'messages';
+  String get tableName => 'messages';
 
   @override
   Map<String, String> fields() => {
@@ -74,8 +74,8 @@ class MessagesTable extends Table {
     'userid': 'INTEGER',
     'content': 'TEXT NOT NULL',
     'createdat': 'TEXT NOT NULL',
-    'FOREIGN KEY (chatid)': 'REFERENCES ${ChatsTable().tableName()}(chatid)',
-    'FOREIGN KEY (userid)': 'REFERENCES ${UsersTable().tableName()}(userid)',
+    'FOREIGN KEY (chatid)': 'REFERENCES ${ChatsTable().tableName}(chatid)',
+    'FOREIGN KEY (userid)': 'REFERENCES ${UsersTable().tableName}(userid)',
   };
 }
 
@@ -85,7 +85,7 @@ class MessagesTable extends Table {
 /// with foreign keys to the 'chats' and 'users' tables.
 class ChatParticipantsTable extends Table {
   @override
-  String tableName() => 'chatparticipants';
+  String get tableName => 'chatparticipants';
 
   @override
   Map<String, String> fields() => {
@@ -94,8 +94,8 @@ class ChatParticipantsTable extends Table {
     'userid': 'INTEGER',
     'role': 'TEXT NOT NULL',
     'joinedat': 'TEXT NOT NULL',
-    'FOREIGN KEY (chatid)': 'REFERENCES ${ChatsTable().tableName()}(chatid)',
-    'FOREIGN KEY (userid)': 'REFERENCES ${UsersTable().tableName()}(userid)',
+    'FOREIGN KEY (chatid)': 'REFERENCES ${ChatsTable().tableName}(chatid)',
+    'FOREIGN KEY (userid)': 'REFERENCES ${UsersTable().tableName}(userid)',
   };
 }
 
@@ -105,7 +105,7 @@ class ChatParticipantsTable extends Table {
 /// with both fields linked to the 'users' table to enforce referential integrity.
 class ContactsTable extends Table {
   @override
-  String tableName() => 'contacts';
+  String get tableName => 'contacts';
 
   @override
   Map<String, String> fields() => {
@@ -114,7 +114,7 @@ class ContactsTable extends Table {
     'userid': 'INTEGER',
     'role': 'TEXT NOT NULL',
     'joinedat': 'TEXT NOT NULL',
-    'FOREIGN KEY (userid)': 'REFERENCES ${UsersTable().tableName()}(userid)',
-    'FOREIGN KEY (contactuserid)': 'REFERENCES ${UsersTable().tableName()}(userid)',
+    'FOREIGN KEY (userid)': 'REFERENCES ${UsersTable().tableName}(userid)',
+    'FOREIGN KEY (contactuserid)': 'REFERENCES ${UsersTable().tableName}(userid)',
   };
 }
