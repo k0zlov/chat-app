0b7dfe3cd4525256074b5bd3132e757322352e2c
Morro <ivankozlov0624@gmail.com>
2024-02-26 17:36:17 +0100

    #6 Some documentation improvements

--- lib/core/supabase/supabase_handler.dart
+++ lib/core/supabase/supabase_handler.dart
@@ -12,20 +12,19 @@ abstract interface class SupabaseHandler {
 
   /// Retrieves data from a specified table in the Supabase database.
   ///
-  /// This method performs a query to fetch data from the given `tableName`,
+  /// This method performs a query to fetch data from the given [tableName],
   /// optionally applying filters, ordering, and limiting the results. It returns
-  /// a `Future` that resolves to an `Either` type, encapsulating a `Failure`
-  /// or the successfully retrieved data of type `T`.
+  /// a [Future] that resolves to an [Either] type, encapsulating a [Failure]
+  /// or the successfully retrieved data of type [T].
   ///
-  /// Parameters:
-  ///   - [tableName]: The name of the table to query data from.
-  ///   - [parser]: A function to convert the JSON map into an instance of `T`.
-  ///   - [orderColumn]: (Optional) The column name to sort the results by.
-  ///   - [filterColumn]: (Optional) The column name to apply the filter on.
-  ///   - [filterValue]: (Optional) The value to filter the results by.
-  ///   - [limit]: The maximum number of results to return (default is 1000).
-  ///   - [filterOperator]: The operator to use for filtering (default is 'eq').
-  ///   - [isAscendingOrder]: Determines the sort order of the results (default is false for descending).
+  ///   - [tableName] : The name of the table to query data from.
+  ///   - [parser] : A function to convert the JSON map into an instance of [T].
+  ///   - [orderColumn] : (Optional) The column name to sort the results by.
+  ///   - [filterColumn] :  (Optional) The column name to apply the filter on.
+  ///   - [filterValue] : (Optional) The value to filter the results by.
+  ///   - [limit] : The maximum number of results to return (default is 1000).
+  ///   - [filterOperator] : The operator to use for filtering (default is 'eq').
+  ///   - [isAscendingOrder] : Determines the sort order of the results (default is false for descending).
   ///
   /// Returns:
   /// A `Future<Either<Failure, T>>` that completes with a `Failure` if an error occurs,
@@ -43,14 +42,14 @@ abstract interface class SupabaseHandler {
 
   /// Inserts data into a specified table in the Supabase database.
   ///
-  /// Takes a generic data of type `T` and inserts it into the specified `tableName`
-  /// after converting it into a JSON map using the provided `parser`. This method
+  /// Takes a generic data of type [T] and inserts it into the specified [tableName]
+  /// after converting it into a JSON map using the provided [parser]. This method
   /// does not return any value but completes with an error if the insertion fails.
   ///
   /// Parameters:
-  ///   - [tableName]: The name of the table to insert data into.
-  ///   - [data]: The data of type `T` to be inserted.
-  ///   - [parser]: A function that converts `T` into a JSON map for insertion.
+  ///   - [tableName] : The name of the table to insert data into.
+  ///   - [data] : The data of type [T] to be inserted.
+  ///   - [parser] : A function that converts [T] into a JSON map for insertion.
   ///
   /// Usage:
   /// This method is intended to be called with the necessary parameters to insert
